0141ed2820e0c5ab74cf4286a8f5aeb2bd27407f
EcoSimIBM <32560271+EcoSimIBM@users.noreply.github.com>
2020-07-31 13:07:15 -0400

    Add files via upload

---
+++ DA-Default/Manipulation.h
@@ -0,0 +1,30 @@
+#ifndef MANIPULATION_H_
+#define MANIPULATION_H_
+
+#include <sstream>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+class Manipulation {
+private:
+
+public:
+
+	Manipulation();
+	virtual ~Manipulation();
+
+	string vtos(float value);
+	string vtos(double value);
+	string vtos(short value);
+	string vtos(int value);
+	string vtos(long int value);
+	string vtos(unsigned long int value);
+	string vtos(long double value);
+	string vtos(unsigned int value);
+	void Tokenize(const string& str, vector<string>& tokens, const string& delimiters = " ");
+	string vtos(unsigned char value);
+};
+
+#endif /* MANIPULATION_H_ */
